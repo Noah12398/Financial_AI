ac0f548da31ebf6311a1532ffc565572b17f6428
Noah12398 <149400947+Noah12398@users.noreply.github.com>
2025-03-01 18:58:55 +0530

    Act20

--- Fin_ai/Adviser/urls.py
+++ Fin_ai/Adviser/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("", views.register, name="register"),
