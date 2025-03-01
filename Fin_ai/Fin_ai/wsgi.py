4f5129361facaef95c7956576e4e77bd80428ea3
Noah12398 <149400947+Noah12398@users.noreply.github.com>
2025-03-01 13:41:34 +0530

    Act13

--- Fin_ai/Fin_ai/wsgi.py
+++ Fin_ai/Fin_ai/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fin_ai.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Fin_ai.settings")
+
 
 application = get_wsgi_application()
